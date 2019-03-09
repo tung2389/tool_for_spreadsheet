f894508b4d33296ee9a7a9d44e43a3522d88e982
tung2389 <luukhanhtung2002@gmail.com>
2019-03-09 17:14:05 +0700

    reorganize code and add function close tabs

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import './App.css';
 import XLSX from 'xlsx';
 import isUrl from 'is-url';
-function valid_url(url)
+let windows = [];
+// Filter to find valid url and open it.
+function process_url(url)
 {
-    if(url !== undefined && isUrl(url))
+    if(url !== undefined)
     {
-        return true;
-    }
-    else
-    {
-        return false;
+        let single_cell = url.toString();
+        if(isUrl(single_cell))
+        {
+            windows.push(window.open(single_cell));
+        }
     }
 }
 class App extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            data:undefined
+            data:undefined,
+            number_of_tabs_each_time:undefined
         };
         this.readData = this.readData.bind(this);
         this.open_according_to_column = this.open_according_to_column.bind(this);
@@ -32,9 +35,7 @@ class App extends React.Component{
         let data = this.state.data;
         for(let i=0;i<data.length;i++)
         {
-            let single_row = data[i][label];
-            if(valid_url(single_row))
-            window.open(single_row.toString());
+            process_url(data[i][label]);
         }
     }
     open_according_to_row(num)
@@ -44,11 +45,14 @@ class App extends React.Component{
         // Loop through all properties of specific_row object
         for(let key in specific_row){
             if (!specific_row.hasOwnProperty(key)) continue;
-            let single_col = specific_row[key];
-            if(valid_url(single_col))
-            {
-                window.open(single_col);
-            }
+            process_url(specific_row[key]);
+        }
+    }
+    close_window()
+    {
+        for(let i=0;i<windows.length;i++)
+        {
+            windows[i].close();
         }
     }
     readData(e)
@@ -65,6 +69,7 @@ class App extends React.Component{
           let json_file = XLSX.utils.sheet_to_json(workbook.Sheets[workbook_name],{header:"A"});
           await this_class.setState({data:json_file});
           alert("The file has been loaded successfully");
+          this_class.open_according_to_row(2);
         };
         if(rABS) reader.readAsBinaryString(f); else reader.readAsArrayBuffer(f);
     }
@@ -72,6 +77,7 @@ class App extends React.Component{
         return(
             <div>
             <input type = "file" onChange = {this.readData}></input>
+            <button onClick = {this.close_window}></button>
             </div>
         );
     }
