e63ae0f462c66208d034c6ee1e1d8c11df2757b9
tung2389 <luukhanhtung2002@gmail.com>
2019-03-10 13:26:52 +0700

    add some interface and caculating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import XLSX from 'xlsx';
 import isUrl from 'is-url';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 let windows = [];
 // Filter to find valid url and open it.
 function process_url(url)
@@ -22,32 +24,78 @@ class App extends React.Component{
             data:undefined,
             number_of_tabs_each_time:undefined,
             time:undefined,
-            hidden:true
+            hidden:true,
+            row_or_column:undefined,
+            status_of_option:0
         };
         this.readData = this.readData.bind(this);
         this.open_according_to_column = this.open_according_to_column.bind(this);
         this.open_according_to_row = this.open_according_to_row.bind(this);
+        this.onChange_tasks = this.onChange_tasks.bind(this);
+        this.onChange_time = this.onChange_time.bind(this);
+        this.onChange_row_or_column = this.onChange_row_or_column.bind(this);
+        this.open_window = this.open_window.bind(this);
+        this.IsEnough = this.IsEnough.bind(this);
+        this.input_of_option = this.input_of_option.bind(this);
+    }
+    IsEnough()
+    {
+        if(this.state.number_of_tabs_each_time && this.state.time && this.state.row_or_column)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     componentDidMount(){
         //console.log(this.state.data);
     }
 
-    open_according_to_column(label)
+    open_according_to_column(label,start,endd)
     {
         let data = this.state.data;
-        for(let i=0;i<data.length;i++)
+        for(let i=start;i<=endd;i++)
         {
             process_url(data[i][label]);
         }
     }
-    open_according_to_row(num)
+    open_according_to_row(num,start,endd)
     {
         let data = this.state.data;
         let specific_row = data[num-1];
+        let cnt = 0;
         // Loop through all properties of specific_row object
         for(let key in specific_row){
-            if (!specific_row.hasOwnProperty(key)) continue;
+            cnt++;
+            // Throw out the key that doesn't belong to object and the key outside the range
+            if (!specific_row.hasOwnProperty(key) || cnt < start || cnt > endd) continue; 
             process_url(specific_row[key]);
+        }
+    }
+    open_window()
+    {
+        if(this.IsEnough())
+        {
+            let num_tabs = this.state.number_of_tabs_each_time;
+            let num_time = this.state.time;
+            let main_data = this.state.row_or_column;
+            //Caculate the range
+            let start = num_time * num_tabs - num_tabs;
+            let endd = num_time * num_tabs - 1;
+            if(this.status_of_option === 0)
+            {
+                this.open_according_to_column(main_data,start,endd);
+            }
+            else
+            {
+                this.open_according_to_row(parseInt(main_data),start,endd);
+            }
+        }
+        else
+        {
+            alert("Missing data from inputs");
         }
     }
     close_window()
@@ -59,11 +107,15 @@ class App extends React.Component{
     }
     onChange_tasks(e)
     {
-        this.setState({number_of_tabs_each_time:e.target.value});
+        this.setState({number_of_tabs_each_time:parseInt(e.target.value)});
     }
     onChange_time(e)
     {
         this.setState({time:parseInt(e.target.value)});
+    }
+    onChange_row_or_column(e)
+    {
+        this.setState({row_or_column:e.target.value});
     }
     readData(e)
     {
@@ -83,6 +135,23 @@ class App extends React.Component{
         };
         if(rABS) reader.readAsBinaryString(f); else reader.readAsArrayBuffer(f);
     }
+    input_of_option()
+    {
+        if(this.state.status_of_option === 0)
+        return (
+            <div className = "div3">
+                <p className = "p1">Enter the label of the column you want to choose.Example: A</p>
+                <TextField label = "Label" onChange = {this.onChange_row_or_column}></TextField>
+            </div>
+        );
+        else
+        return(
+            <div className = "div3">
+                <p className = "p1">Enter the number of row you want to choose. Example: 1</p>
+                <TextField label = "number of row"> onChange = {this.onChange_row_or_column}</TextField>
+            </div>
+        );
+    }
     render(){
         return(
             <div>
@@ -94,8 +163,13 @@ class App extends React.Component{
             <button onClick = {this.Change_page2} className = "btn_page2">Open according to row</button>
 
             {/* Two parameter to open */}
-            <input type = "text" onChange = {this.onChange_tasks} className = "inp1"></input>
-            <input type = "number" onChange = {this.onChange_time} className = "inp2"></input>
+            {this.input_of_option()}
+            <div className = "div1">
+                <TextField type = "number" onChange = {this.onChange_tasks} label = "Number of tabs each time" className = "inp1"></TextField>
+            </div>
+            <div className = "div2">
+                <TextField type = "number" onChange = {this.onChange_time} label = "Number of time" className = "inp2"></TextField>
+            </div>
 
             {/* Button to open and close window*/}
             <button>Open</button>
