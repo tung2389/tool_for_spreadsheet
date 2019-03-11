3643d121196b4e189a5838912ccbb5e64f605290
tung2389 <36613569+tung2389@users.noreply.github.com>
2019-03-11 16:52:17 +0700

    upload production

---
+++ precache-manifest.00ba258739b2140d303ebcd135b40b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "5c3054db1766a9fed161",
+    "url": "./static/js/main.5c3054db.chunk.js"
+  },
+  {
+    "revision": "485fb40ce6ef31840069",
+    "url": "./static/js/2.485fb40c.chunk.js"
+  },
+  {
+    "revision": "5c3054db1766a9fed161",
+    "url": "./static/css/main.f41e72da.chunk.css"
+  },
+  {
+    "revision": "662db06208171d8bf7383f066954c7e1",
+    "url": "./index.html"
+  }
+];
